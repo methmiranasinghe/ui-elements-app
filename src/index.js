d6549ea1814546dc154d96cd41646619f360757b
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-01-11 02:28:04 +0530

    blog posts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 // import 'semantic-ui-css/semantic.min.css'
 import ReactDOM from 'react-dom';
-
+import pic1 from './images/pic1.jpg'
+import pic2 from './images/pic2.jpg'
+import pic3 from './images/pic3.jpg'
 const App = () =>{
     return(
         <div className='ui comments'>
             <div className='comment'>
                 <a href="/" className='avatar'>
-                    <img  alt="profile-pic"/>
+                    <img src={pic1} alt="profile-pic"/>
                 </a>
                 <div className='content'>
                     <a href="/" className='author'>
-                        Sarah
+                        John Hans
                     </a>
                     <div className='meta-data'>
                         <span className='date'>
@@ -19,7 +21,43 @@ const App = () =>{
                         </span>
                     </div>
                     <div className='text'>
-                        it's amazing
+                        It's amazing
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <a href="/" className='avatar'>
+                    <img src={pic2} alt="profile-pic"/>
+                </a>
+                <div className='content'>
+                    <a href="/" className='author'>
+                        Marry Jane
+                    </a>
+                    <div className='meta-data'>
+                        <span className='date'>
+                            Today at 7:08PM
+                        </span>
+                    </div>
+                    <div className='text'>
+                        It's Great Experience
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <a href="/" className='avatar'>
+                    <img src={pic3} alt="profile-pic"/>
+                </a>
+                <div className='content'>
+                    <a href="/" className='author'>
+                        Lorry Ganil
+                    </a>
+                    <div className='meta-data'>
+                        <span className='date'>
+                            Today at 3:08AM
+                        </span>
+                    </div>
+                    <div className='text'>
+                        It's a nice job!
                     </div>
                 </div>
             </div>
