f919d7c3e7d14ba32b32a9845321776109c8fdaf
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-01-11 03:30:05 +0530

    images with props

--- src/index.js
+++ src/index.js
@@ -1,67 +1,31 @@
 import React from 'react';
-// import 'semantic-ui-css/semantic.min.css'
 import ReactDOM from 'react-dom';
-import pic1 from './images/pic1.jpg'
+import pic1 from './images/pic1.jpg';
 import pic2 from './images/pic2.jpg'
 import pic3 from './images/pic3.jpg'
+import SingleComment from './SingleComment';
+
 const App = () =>{
     return(
         <div className='ui comments'>
-            <div className='comment'>
-                <a href="/" className='avatar'>
-                    <img src={pic1} alt="profile-pic"/>
-                </a>
-                <div className='content'>
-                    <a href="/" className='author'>
-                        John Hans
-                    </a>
-                    <div className='meta-data'>
-                        <span className='date'>
-                            Today at 5:08PM
-                        </span>
-                    </div>
-                    <div className='text'>
-                        It's amazing
-                    </div>
-                </div>
-            </div>
-            <div className='comment'>
-                <a href="/" className='avatar'>
-                    <img src={pic2} alt="profile-pic"/>
-                </a>
-                <div className='content'>
-                    <a href="/" className='author'>
-                        Marry Jane
-                    </a>
-                    <div className='meta-data'>
-                        <span className='date'>
-                            Today at 7:08PM
-                        </span>
-                    </div>
-                    <div className='text'>
-                        It's Great Experience
-                    </div>
-                </div>
-            </div>
-            <div className='comment'>
-                <a href="/" className='avatar'>
-                    <img src={pic3} alt="profile-pic"/>
-                </a>
-                <div className='content'>
-                    <a href="/" className='author'>
-                        Lorry Ganil
-                    </a>
-                    <div className='meta-data'>
-                        <span className='date'>
-                            Today at 3:08AM
-                        </span>
-                    </div>
-                    <div className='text'>
-                        It's a nice job!
-                    </div>
-                </div>
-            </div>
+            <SingleComment 
+            name='John Hans' 
+            date='1 Jan at 6.00PM' 
+            post='Great Post' 
+            pic={pic1}/>
+            
+            <SingleComment 
+            name ='Marry Jane' 
+            date=' 8 Jan at 8.00PM' 
+            post='Nice Experience' 
+            pic={pic2} />
 
+            <SingleComment 
+            name='Jane Erw' 
+            date='Today at 9.00PM' 
+            post="It's Amazing !" 
+            pic={pic3}/>
+           
         </div>
     )
 }
